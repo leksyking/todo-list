2a55a65a07e91685da7b75c3ffc4006f41ecfdd7
Felix <gbemilekeogundipe@gmail.com>
2021-10-26 00:50:29 -0700

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -106,9 +106,13 @@ app.post("/delete", (req,res) => {
             }
         })
     }
-})
+});
 
-app.listen(3000, () => {
-    console.log('Starting App!'); 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, () => {
+    console.log('Server has started successfully.'); 
 });
  
